6a8ab259ee9d0a5ce615b805662f488950b82007
akonar-dev <157737602+akonar-dev@users.noreply.github.com>
2025-02-22 08:41:11 +0530

    Passing prop name and age

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Greetings from './components/Greetings'
+import UserInfo from './components/UserInfo'
 
 function App() {
 
   return (
     <>
     <Greetings/>
+    <UserInfo username={"Arijeet"} age={27}/>
     </>
   )
 }
---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,11 @@
+
+const UserInfo = ({username, age}) => {
+  return (
+    <div>
+        <h2>Username : {username}</h2>
+        <h2>Age : {age}</h2>
+    </div>
+  )
+}
+
+export default UserInfo
